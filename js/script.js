f4f8bb06abf70c94bf1f291e23deb5a3bc406b9e
Eugen <link12311@gmail.com>
2021-08-23 19:34:13 +0300

    first homework

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5`);
+let numbersOfFilms = +prompt('Сколько фильмов выуже посмотрели?', '');
+
+let personalMovieDB = {
+    counter: numbersOfFilms,
+    movies: {},
+    actors: '',
+    genres: [],
+    privat: false
+};
+
+let lastMovie = prompt('Один из последних просмотренных фильмов?', ''),
+    lastScore = prompt('На сколько оцените его?', ''),
+    lastMovieA = prompt('Один из последних просмотренных фильмов?', ''),
+    lastScoreA = prompt('На сколько оцените его?', '');
+
+
+    personalMovieDB.movies[lastMovie] = lastScore;
+    personalMovieDB.movies[lastMovieA] = lastScoreA;
+
+    console.log(personalMovieDB);
+
