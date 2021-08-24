2d500f0188ddd6ca9be3bf240ae51e5806270ee9
Eugen <link12311@gmail.com>
2021-08-24 18:32:08 +0300

    learned the function and optimized the project

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
-let numbersOfFilms = +prompt("Сколько фильмов выуже посмотрели?", "");
+let numbersOfFilms;
+
+function start() {
+  numbersOfFilms = +prompt("Сколько фильмов выуже посмотрели?", "");
+  while (
+    numbersOfFilms == "" ||
+    numbersOfFilms == null ||
+    isNaN(numbersOfFilms)
+  ) {
+    numbersOfFilms = +prompt("Сколько фильмов выуже посмотрели?", "");
+  }
+}
+
+start();
 
 let personalMovieDB = {
   counter: numbersOfFilms,
@@ -8,24 +21,56 @@ let personalMovieDB = {
   privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-  let a = prompt("Один из последних просмотренных фильмов?", ""),
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Один из последних просмотренных фильмов?", ""),
       b = prompt("На сколько оцените его?", "");
-  if (a && b != null && a && b != "" && a.length && b.length < 50) {
-    personalMovieDB.movies[a] = b;
-  } else {
-    i--;
+    if (a && b != null && a && b != "" && a.length && b.length < 50) {
+      personalMovieDB.movies[a] = b;
+    } else {
+      i--;
+    }
   }
 }
 
-if(personalMovieDB.counter <= 10){
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.counter <= 10) {
     console.log("Вы смотрели слишком мало фильмов!");
-} else if(personalMovieDB.counter >= 30){
+  } else if (personalMovieDB.counter >= 30) {
     console.log("Вы классический зритель");
-} else if(personalMovieDB.counter > 30) {
+  } else if (personalMovieDB.counter > 30) {
     console.log("Вы киноман");
-}else {
+  } else {
     console.log("Произошла ошибка");
+  }
 }
+
+detectPersonalLevel();
+
+function showMyDB(private) {
+    if(!private) {
+        console.log(personalMovieDB.privat);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for(let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
+
+
+
+
+
+
+
+
 
 console.log(personalMovieDB);
