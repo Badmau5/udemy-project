3b03088c7e22d4f08b0be35196a5c06833e6ff4e
Eugen <link12311@gmail.com>
2021-08-24 17:37:16 +0300

    second homework update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
-let numbersOfFilms = +prompt('Сколько фильмов выуже посмотрели?', '');
+let numbersOfFilms = +prompt("Сколько фильмов выуже посмотрели?", "");
 
 let personalMovieDB = {
-    counter: numbersOfFilms,
-    movies: {},
-    actors: '',
-    genres: [],
-    privat: false
+  counter: numbersOfFilms,
+  movies: {},
+  actors: "",
+  genres: [],
+  privat: false,
 };
 
-let lastMovie = prompt('Один из последних просмотренных фильмов?', ''),
-    lastScore = prompt('На сколько оцените его?', ''),
-    lastMovieA = prompt('Один из последних просмотренных фильмов?', ''),
-    lastScoreA = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  let a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", "");
+  if (a && b != null && a && b != "" && a.length && b.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
 
+if(personalMovieDB.counter <= 10){
+    console.log("Вы смотрели слишком мало фильмов!");
+} else if(personalMovieDB.counter >= 30){
+    console.log("Вы классический зритель");
+} else if(personalMovieDB.counter > 30) {
+    console.log("Вы киноман");
+}else {
+    console.log("Произошла ошибка");
+}
 
-    personalMovieDB.movies[lastMovie] = lastScore;
-    personalMovieDB.movies[lastMovieA] = lastScoreA;
-
-    console.log(personalMovieDB);
-
+console.log(personalMovieDB);
